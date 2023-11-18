52a22bc9e9e980ecd1d6ee9ced652700b282cfe4
zeno <tmp-git@groundzeno.net>
2023-11-18 05:35:29 +0100

    add enemy spawner

--- TInk_Jam_2023/Assets/Scripts/Spawner/Spawner.cs
+++ TInk_Jam_2023/Assets/Scripts/Spawner/Spawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class SpawnBehaviour {
 	public GameObject enemyType;
+	public int amount;
+	public bool repeat;
 	public int intervalSec;
 }
 
@@ -13,6 +15,7 @@ public class Spawner : MonoBehaviour
 	[SerializeField]
 	private int spawnDelaySec = 0;
 
+	// TODO(adrian): actually implement this
 	[SerializeField]
 	private int spawnDurationSec;
 	[SerializeField]
@@ -24,17 +27,37 @@ public class Spawner : MonoBehaviour
 	[SerializeField]
 	private Timer timer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-		timer.delay = spawnDelaySec;
-		timer.onTimerDone.AddListener(() => { Debug.Log("timer done"); });
-		StartCoroutine(timer.StartTimer());
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		Timer spawnDelayTimer = Instantiate(timer, this.transform);
+		spawnDelayTimer.delay = spawnDelaySec;
+		spawnDelayTimer.onTimerDone.AddListener(() => { StartSpawnRoutine(); });
+		StartCoroutine(spawnDelayTimer.StartTimer());
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void StartSpawnRoutine() {
+		foreach (SpawnBehaviour behaviour in spawnBehaviours) {
+			StartCoroutine(SpawnBehaviourHandler(behaviour));
+		}
+	}
+
+	IEnumerator SpawnBehaviourHandler(SpawnBehaviour behaviour) {
+		Timer spawnTimer = Instantiate(timer, this.transform);
+		spawnTimer.repeat = behaviour.repeat;
+		spawnTimer.delay = behaviour.intervalSec;
+		spawnTimer.onTimerDone.AddListener(() => {
+			for (int i = 0; i < behaviour.amount; i++) {
+				Instantiate(behaviour.enemyType, this.transform.position, Quaternion.identity);
+			}
+		});
+		StartCoroutine(spawnTimer.StartTimer());
+		yield return null;
+	}
 }
