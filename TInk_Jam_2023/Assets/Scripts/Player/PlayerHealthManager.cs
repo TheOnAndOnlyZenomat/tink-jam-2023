98473cc9d75966fe923ac33107352d1d6f47b730
AlcaCodeX <dang.kevin0112@gmail.com>
2023-11-19 00:56:34 +0100

    Level design finished and Player death added

--- TInk_Jam_2023/Assets/Scripts/Player/PlayerHealthManager.cs
+++ TInk_Jam_2023/Assets/Scripts/Player/PlayerHealthManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
@@ -15,6 +16,16 @@ public class PlayerHealthManager : MonoBehaviour
 
     public void PlayerTakeDamage(int damage)
     {
-        _currentHealth = _currentHealth - damage;
+        _currentHealth -= damage;
+
+        if (_currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Debug.Log("Player has died");
     }
 }
