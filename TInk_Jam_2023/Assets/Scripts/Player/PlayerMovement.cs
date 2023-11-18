c3faeafdc22a56ca8136ce53bfd57b0af2d47744
AlcaCodeX <dang.kevin0112@gmail.com>
2023-11-18 18:11:17 +0100

    Player Dash working

--- TInk_Jam_2023/Assets/Scripts/Player/PlayerMovement.cs
+++ TInk_Jam_2023/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _dashingPower = 15f;
+    [SerializeField] private float _dashingTime = 0.2f;
+    [SerializeField] private float _dashingCooldown = 1f;
     private Vector2 _movementInput;
     private Rigidbody2D _rb;
+    private bool _canDash = true;
+    private bool _isDashing;
+    
+    
 
     private void Awake()
     {
@@ -14,6 +22,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_isDashing)
+        {
+            return;
+        }
         _rb.velocity = _movementInput * _speed;
     }
 
@@ -21,5 +33,21 @@ public class PlayerMovement : MonoBehaviour
     {
         _movementInput = inputValue.Get<Vector2>();
     }
-}
 
+    private void OnDash()
+    {
+        if(_canDash)
+            StartCoroutine(Dash());
+    }
+
+    private IEnumerator Dash()
+    {
+        _canDash = false;
+        _isDashing = true;
+        _rb.velocity = _movementInput * _dashingPower;
+        yield return new WaitForSeconds(_dashingTime);
+        _isDashing = false;
+        yield return new WaitForSeconds(_dashingCooldown);
+        _canDash = true;
+    }
+}
